9cad332a3b679538311afe22069e0b0e0bf34b45
Daniilas <47268986+DaniilasIvanovas@users.noreply.github.com>
2019-05-31 15:42:17 +0300

    Header fixes at Index.hmtl
    Fixed header not disappearing immediately

--- popup-animation.js
+++ popup-animation.js
@@ -1,21 +1,10 @@
 $(document).ready(function () {
     $(".show").mouseover(function () {
-      // $(".appear").hide(10);
-      $(".show").slideUp(1000);
+      $(".show").hide();
       $(".appear").slideDown(1000);
-      $(".show").slideUp(1000);
     });
     $(".x").click(function () {
       $(".appear").slideUp(1000);
       $(".show").slideDown(1000);
     });
-//     $('.arrow'.click(function(){
-//         // if ($('.slide').css('display') == 'block'){
-//         //     $(".appear").slideUp(1000);
-//         //     $(".show").slideDown(1000);
-//         // }else {
-//         //     alert('lol');
-//         // }
-//         alert("lol");
-//     });
 });
